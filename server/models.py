60aa703e27521c48729284b369d6160f988f7cb8
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-20 22:27:48 +0530

    updated sqlalchemy models

--- server/models.py
+++ server/models.py
@@ -1,4 +1,9 @@
 
+from datetime import UTC , datetime 
+from time import timezone
+from typing import Text
+from sqlalchemy import String , DateTime
+from sqlalchemy import Integer
 from sqlalchemy.orm import DeclarativeBase , Mapped , mapped_column , relationship , ForeignKey
 
 
@@ -9,21 +14,31 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
 
-    id : Mapped[int] = mapped_column( primary_key=True , index=True , autoincrement=True)
-    username : Mapped[str] = mapped_column( unique=True , nullable=False )
-    email : Mapped[str] = mapped_column(unique=True , nullable=False)
-    hashed_password : Mapped[str] = mapped_column( nullable=False )
+    id : Mapped[int] = mapped_column(Integer , primary_key=True , index=True , autoincrement=True)
+    username : Mapped[str] = mapped_column(String(50), unique=True , nullable=False )
+    email : Mapped[str] = mapped_column( String(120),unique=True , nullable=False)
+    image_file : Mapped[str | None] = mapped_column( String(255) , nullable=True , default= "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" )
     
     blogs : Mapped[list["Blog"]] =  relationship(back_populates="author")
+
+    @property
+    def image_path(self) -> str:
+        if self.image_file:
+            return f"/static/profile_pics/{self.image_file}"
+        
+        return "/static/profile_pics/default.jpg"
 
 
 class Blog(Base):
     __tablename__ = "blogs"
 
-    id : Mapped[int] = mapped_column(primary_key=True , index=True , autoincrement=True)
-    title : Mapped[str] = mapped_column( unique=True , nullable=False )
-    content : Mapped[str] = mapped_column( nullable=False )
-    author_id : Mapped[int] = mapped_column( ForeignKey(User.id) , nullable=False )
+    id : Mapped[int] = mapped_column(Integer,primary_key=True , index=True , autoincrement=True)
+    title : Mapped[str] = mapped_column( String(100), unique=True , nullable=False )
+    content : Mapped[str] = mapped_column( Text, nullable=False )
+    author_id : Mapped[int] = mapped_column( ForeignKey(User.id) , nullable=False , index = True)
+    date_posted : Mapped[datetime] = mapped_column( DateTime(timezone = True) , default = lambda: datetime.now(UTC)) 
+
+
 
     author : Mapped["User"] = relationship(back_populates="blogs")
     
--- server/schemas.py
+++ server/schemas.py
@@ -1,12 +1,22 @@
 
-from pydantic import BaseModel , ConfigDict , Field
+from pydantic import BaseModel , ConfigDict , Field , EmailStr
 
+from datetime import datetime , UTC
 
+class UserBase(BaseModel):
+    username : str = Field(min_length=4 , max_length=20)
+    email : EmailStr = Field(max_length= 20)
+    
 
+class UserCreate(UserBase):
+    hashed_password : str = Field(min_length=6 , max_length=20)
+
+class UserResponse(UserBase):
+    id : int 
+    
 
 
 class BlogBase(BaseModel):
-    id : int
     title : str = Field(min_length=1 , max_length=100)
     author : str = Field(min_length=1 , max_length=50)
     content : str = Field(min_length=1 , max_length=1000)
